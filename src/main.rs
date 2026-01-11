58ee048074bece5d2e534393a58f0e548ee644e3
Swarnit <ghuleswarnit@gmail.com>
2026-01-11 10:30:34 +0000

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+struct Graph {
+    adj_list: Vec<Vec<usize>>,
+    vectors: Vec<Vec<f32>>,
 }
+
+impl Graph {
+    fn add_node() {}
+
+    fn greedy_search() {}
+}
+
+fn main() {}
